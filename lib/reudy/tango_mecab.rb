35992e0eddce5affc4698f8346317c2bf6879824
krororo <heavens.feel.7@gmail.com>
2018-02-12 23:09:49 +0900

    rubocop: Style/MutableConstant

--- lib/reudy/tango_mecab.rb
+++ lib/reudy/tango_mecab.rb
@@ -5,7 +5,7 @@ require 'MeCab'
 class WordExtractor
   # WordExtractor(単語候補リストを保持する長さ,単語追加時のコールバック)
 
-  POS_ID = [38, 41, 42, 43, 44, 45, 46, 47] # 単語として扱う品詞リスト
+  POS_ID = [38, 41, 42, 43, 44, 45, 46, 47].freeze # 単語として扱う品詞リスト
 
   def initialize(_candlistlength = 7, onaddword = nil)
     @onAddWord = onaddword
